8df46ea7bc5656df70b73cd9d36f28e3dc472097
Justas T <justas.tomkus@sneakybox-studios.com>
2019-01-26 19:04:08 +0200

    wonky enemy AI movement

--- GameJam2019_BabyDemon/Assets/Scripts/Settings/Settings.cs
+++ GameJam2019_BabyDemon/Assets/Scripts/Settings/Settings.cs
@@ -1,4 +1,5 @@
 ﻿using DB;
+using DB.Enemy;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "GameSettings.asset", menuName = "CreateGameSettings")]
@@ -7,6 +8,7 @@ public class Settings : ScriptableObject
 	public PlayerMovement.Config PlayerMovementSettings;
 	public CharacterInteraction.Config InteractionSettings;
 	public CharacterControls.Config ControlSettings;
+	public Enemy EnemySettings;
 
 	private const string defaultLocation = "GameSettings";
 	private static Settings _settings;
@@ -14,6 +16,13 @@ public class Settings : ScriptableObject
 	[System.Serializable]
 	public class GameplayConfig
 	{
+	}
+
+	[System.Serializable]
+	public class Enemy
+	{
+		public EnemyMovement.Config Movement;
+		public EnemyAI.Config AI;
 	}
 
 	public static Settings Get
